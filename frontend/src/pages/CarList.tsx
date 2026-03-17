c8b9903590f2bee2cb5445ffa9542866f90ef5ce
Daniel <danikrustanov@gmail.com>
2026-03-17 10:42:48 +0200

    Added icons/buttons for sorting and filter

--- frontend/src/pages/CarList.tsx
+++ frontend/src/pages/CarList.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import type { RootState } from '../stores/store';
 import { getCars } from '../services/carsService';
-import { setCars, setLoading, setError, clearError} from '../stores/carsSlice';
+import { setCars, setLoading, setError, clearError } from '../stores/carsSlice';
 import Pagination from '../components/pagePagination';
 import CarCard from '../components/Car/CarCard';
 import EmptyState from '../components/UX/EmptyState';
@@ -13,6 +13,8 @@ import Transmission from '../components/Filters/Transmission';
 import Mileage from '../components/Filters/Mileage';
 import matchesFilters from './Helpers/matchFilters';
 import { SkeletonLoader } from '../components/UX/SkeletonLoader';
+import { FaFilter } from 'react-icons/fa';
+import {LuArrowUpDown} from 'react-icons/lu';
 
 
 
@@ -26,8 +28,8 @@ function CarList() {
   const [mileageFilter, setMileageFilter] = useState("Mileage");
 
 
- const filteredCars = cars.filter((car) => 
-    matchesFilters(car, {fuelType, transmission, mileageFilter})
+  const filteredCars = cars.filter((car) =>
+    matchesFilters(car, { fuelType, transmission, mileageFilter })
   );
 
   const [page, setPage] = useState(1);
@@ -57,137 +59,145 @@ function CarList() {
     setPage(1);
   }
 
-    useEffect(() => {
-      const handler = setTimeout(() => {
-        setDebouncedFilters(yearFilters)
-      }, 1000)
-      return () => clearTimeout(handler)
-    }, [yearFilters])
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedFilters(yearFilters)
+    }, 1000)
+    return () => clearTimeout(handler)
+  }, [yearFilters])
 
-    const fetchCars = async () => {
-        dispatch(setLoading(true));
-        dispatch(clearError());
+  const fetchCars = async () => {
+    dispatch(setLoading(true));
+    dispatch(clearError());
 
-        try {
-          const min = debouncedFilters.min ? parseInt(debouncedFilters.min) : undefined;
-          const max = debouncedFilters.max ? parseInt(debouncedFilters.max) : undefined;
-          const response = await getCars(page, 9, min, max);
-          dispatch(setCars(response.items ?? []));
-          setTotalPages(response.totalPages ?? 0);
-        } catch {
-          dispatch(setError("Unable to load cars."));
-        } finally {
-          dispatch(setLoading(false));
-        }
-      };
-
-    useEffect(() => {
-      if (page != null)
-        fetchCars();
-    }, [page, dispatch, debouncedFilters])
-
-    if (loading) {
-      return (
-        <div className="min-h-screen bg-slate-900 font-sans p-6 md:p-12 pt-5">
-          <div className="flex justify-center m-5">
-            <SkeletonLoader type="details" count={3}/>
-          </div>
-        </div>
-      );
+    try {
+      const min = debouncedFilters.min ? parseInt(debouncedFilters.min) : undefined;
+      const max = debouncedFilters.max ? parseInt(debouncedFilters.max) : undefined;
+      const response = await getCars(page, 9, min, max);
+      dispatch(setCars(response.items ?? []));
+      setTotalPages(response.totalPages ?? 0);
+    } catch {
+      dispatch(setError("Unable to load cars."));
+    } finally {
+      dispatch(setLoading(false));
     }
-    if (error) {
-      return (
-        <div className="min-h-screen bg-slate-900 font-sans p-6 md:p-12 pt-20">
-          <div className="max-w-7xl mx-auto bg-red-500/10 border border-red-500 text-red-500 p-4 rounded-xl mb-6">
-            {error}
-          </div>
-        </div>
-      );
-    }
+  };
 
-    if (!loading && !error && cars.length === 0) {
-      return (
-        <EmptyState />
-      );
-    }
+  useEffect(() => {
+    if (page != null)
+      fetchCars();
+  }, [page, dispatch, debouncedFilters])
 
+  if (loading) {
     return (
-      <div className="min-h-screen bg-slate-900 font-sans p-6 md:p-6 pt-20">
-        <div className="max-w-7xl mx-auto">
-
-          <div className="mb-5 text-center">
-            <h1 className="text-4xl font-black text-white tracking-tight mb-2">
-              Explore Our <span className="text-[#70FFE2]">Fleet</span>
-            </h1>
-            <p className="text-slate-400">Discover the perfect ride for your next journey.</p>
-
-          </div>
-
-          <div className="flex items-center gap-3 mb-4"> 
-
-            <span className="text-slate-400 text-sm font-medium whitespace-nowrap ml-2">
-              Fuel type
-            </span>
-            <FuelType
-              value={fuelType}
-              onChange={(val) => setFuelType(val)}
-            />
-
-             <span className="text-slate-400 text-sm font-medium whitespace-nowrap ml-2">
-              Transmission
-            </span>
-            <Transmission
-              value={transmission}
-              onChange={(val) => setTransmission(val)}
-            />
-
-             <span className="text-slate-400 text-sm font-medium whitespace-nowrap ml-2">
-              Mileage
-            </span>
-            <Mileage
-              value={mileageFilter}
-              onChange={(val) => setMileageFilter(val)}
-            />
-
-            <div className="flex items-center gap-3 mb-4">
-              <span className="text-slate-400 text-sm font-medium whitespace-nowrap ml-2">
-                Sort by:
-              </span>
-              <SortedCars
-                value={sortType}
-                onChange={(val) => setSortType(val)}
-              />
-            </div>
-
-
-            <div className="flex items-center gap-3 mb-4">
-              <span className="text-slate-400 text-sm font-medium whitespace-nowrap ml-2">
-                Year:
-              </span>
-              <YearRangeFilter
-                value={yearFilters}
-                onFilterChange={handleFilterChange}
-              />
-            </div>
-          </div>
-
-
-
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {sortedCars.map((car) => (
-              <CarCard key={car.id} car={car} showDeletebutton={false} />
-            ))}
-          </div>
-          <Pagination
-            currentPage={page}
-            totalPages={totalPages}
-            onPageChange={(newPage: number) => {
-              setPage(newPage);
-              window.scrollTo({ top: 0, behavior: "smooth" });
-            }}
-          />
+      <div className="min-h-screen bg-slate-900 font-sans p-6 md:p-12 pt-5">
+        <div className="flex justify-center m-5">
+          <SkeletonLoader type="details" count={3} />
         </div>
       </div>
     );
   }
+  if (error) {
+    return (
+      <div className="min-h-screen bg-slate-900 font-sans p-6 md:p-12 pt-20">
+        <div className="max-w-7xl mx-auto bg-red-500/10 border border-red-500 text-red-500 p-4 rounded-xl mb-6">
+          {error}
+        </div>
+      </div>
+    );
+  }
+
+  if (!loading && !error && cars.length === 0) {
+    return (
+      <EmptyState />
+    );
+  }
+
+  return (
+    <div className="min-h-screen bg-slate-900 font-sans p-6 md:p-6 pt-20">
+      <div className="max-w-7xl mx-auto">
+
+        <div className="mb-5 text-center">
+          <h1 className="text-4xl font-black text-white tracking-tight mb-2">
+            Explore Our <span className="text-[#70FFE2]">Fleet</span>
+          </h1>
+          
+          <p className="text-slate-400">Discover the perfect ride for your next journey.</p>
+
+        </div>
+
+        <div className="flex items-center gap-3 mb-4">
+          <button className="flex items-center gap-2 p-2 bg-slate-800 rounded-lg text-white">
+            <FaFilter size={16} />
+            <span></span>
+          </button>
+          <button className="flex items-center gap-2 p-2 bg-slate-800 rounded-lg text-white">
+            <LuArrowUpDown size={16} />
+            <span></span>
+          </button>
+          <span className="text-slate-400 text-sm font-medium whitespace-nowrap ml-2">
+            Fuel type
+          </span>
+          <FuelType
+            value={fuelType}
+            onChange={(val) => setFuelType(val)}
+          />
+
+          <span className="text-slate-400 text-sm font-medium whitespace-nowrap ml-2">
+            Transmission
+          </span>
+          <Transmission
+            value={transmission}
+            onChange={(val) => setTransmission(val)}
+          />
+
+          <span className="text-slate-400 text-sm font-medium whitespace-nowrap ml-2">
+            Mileage
+          </span>
+          <Mileage
+            value={mileageFilter}
+            onChange={(val) => setMileageFilter(val)}
+          />
+
+          <div className="flex items-center gap-3 mb-4">
+            <span className="text-slate-400 text-sm font-medium whitespace-nowrap ml-2">
+              Sort by:
+            </span>
+            <SortedCars
+              value={sortType}
+              onChange={(val) => setSortType(val)}
+            />
+          </div>
+
+
+          <div className="flex items-center gap-3 mb-4">
+            <span className="text-slate-400 text-sm font-medium whitespace-nowrap ml-2">
+              Year:
+            </span>
+            <YearRangeFilter
+              value={yearFilters}
+              onFilterChange={handleFilterChange}
+            />
+          </div>
+        </div>
+
+
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {sortedCars.map((car) => (
+            <CarCard key={car.id} car={car} showDeletebutton={false} />
+          ))}
+        </div>
+        <Pagination
+          currentPage={page}
+          totalPages={totalPages}
+          onPageChange={(newPage: number) => {
+            setPage(newPage);
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+        />
+      </div>
+    </div>
+  );
+}
 export default CarList
