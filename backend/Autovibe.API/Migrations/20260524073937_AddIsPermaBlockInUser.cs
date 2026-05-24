b07eaa9ee6bbd1ace79a43111fa4127505b44d46
Daniel <danikrustanov@gmail.com>
2026-05-24 10:40:23 +0300

    created migrations to database

---
+++ backend/Autovibe.API/Migrations/20260524073937_AddIsPermaBlockInUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Autovibe.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsPermaBlockInUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
