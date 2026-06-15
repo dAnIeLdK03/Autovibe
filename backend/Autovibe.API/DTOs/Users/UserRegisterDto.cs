02ef196e0147a63e3fb0004cf70e719053b2b47a
Daniel <danikrustanov@gmail.com>
2026-06-15 09:29:09 +0300

    Remove ConfirmPassword from DTOs as password confirmation is handled on the client

--- backend/Autovibe.API/DTOs/Users/UserRegisterDto.cs
+++ backend/Autovibe.API/DTOs/Users/UserRegisterDto.cs
@@ -8,8 +8,6 @@ public class UserRegisterDto
 
     public string Password {get; set; } = string.Empty;
 
-    public string ConfirmPassword {get; set; } = string.Empty;
-
     public string? FirstName {get; set; }
     public string? LastName {get; set; }
     public string? PhoneNumber {get; set; }
