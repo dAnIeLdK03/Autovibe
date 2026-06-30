8fd62848349b2743f09173534eb6ac89bdd8b6b3
Daniel <danikrustanov@gmail.com>
2026-06-30 13:13:52 +0300

    Make FirstName and LastName required non-nullable fields in UserRegisterDto

--- backend/Autovibe.API/DTOs/Users/UserRegisterDto.cs
+++ backend/Autovibe.API/DTOs/Users/UserRegisterDto.cs
@@ -8,8 +8,8 @@ public class UserRegisterDto
 
     public string Password {get; set; } = string.Empty;
 
-    public string? FirstName {get; set; }
-    public string? LastName {get; set; }
+    public string FirstName {get; set; } = string.Empty;
+    public string LastName {get; set; } = string.Empty;
     public string? PhoneNumber {get; set; }
 
 
